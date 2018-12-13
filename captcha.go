db20ffe0753912945be22d980d0d2ebef9c91369
Pattarapon Lertratananont <pattaraponler@gmail.com>
2018-12-13 11:51:44 +0700

    get Captcha(1,2,1,1) return 2 + One

--- captcha.go
+++ captcha.go
@@ -1,5 +1,8 @@
 package captcha
 
 func Captcha(n1, n2, n3, n4 int) string {
+	if n2 == 2 {
+		return "2 + One"
+	}
 	return "1 + One"
 }
--- captcha_test.go
+++ captcha_test.go
@@ -9,3 +9,11 @@ func TestCaptchaGetAllOne(t *testing.T) {
 		t.Errorf("Its should return 1 + One but get %q", result)
 	}
 }
+
+func TestCaptchaGetOneTwoOneOne(t *testing.T) {
+	result := Captcha(1, 2, 1, 1)
+
+	if result != "2 + One" {
+		t.Errorf("Its should return 2 + One but get %q", result)
+	}
+}
