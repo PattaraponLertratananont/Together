8d3f995f30e2574d9635a242533ad7373218a80a
Pattarapon Lertratananont <pattaraponler@gmail.com>
2018-12-13 14:21:06 +0700

    refector code captcha.go remove all if and return letf + One

--- captcha.go
+++ captcha.go
@@ -1,32 +1,9 @@
 package captcha
 
+import (
+	"strconv"
+)
+
 func Captcha(pattern, left, operator, right int) string {
-	if left == 9 {
-		return "9 + One"
-	}
-	if left == 8 {
-		return "8 + One"
-	}
-	if left == 7 {
-		return "7 + One"
-	}
-	if left == 6 {
-		return "6 + One"
-	}
-	if left == 5 {
-		return "5 + One"
-	}
-	if left == 4 {
-		return "4 + One"
-	}
-	if left == 3 {
-		return "3 + One"
-	}
-	if left == 2 {
-		return "2 + One"
-	}
-	if left == 1 {
-		return "1 + One"
-	}
-	return "0 + One"
+	return strconv.Itoa(left) + " + One"
 }
