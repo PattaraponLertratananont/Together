a86410edf59dc71d835350275c779ed141a33bd3
tammarut <tammarut>
2018-12-18 17:24:23 +0700

    [production code]: create func printFor and call func on main pass input by PassvalueRef.

--- Learning/Pleum/Chapter6/main.go
+++ Learning/Pleum/Chapter6/main.go
@@ -3,15 +3,17 @@ package main
 //เขียนโปรแกรมinputเลข 1ตัว แล้วprintตั้งแต่1ถึงเลขที่ใส่
 import "fmt"
 
+func printFor(input *int) {
+	for i := 1; i <= *input; i++ {
+		fmt.Printf("%d ", i)
+	}
+}
 func main() {
 	var input int
 
 	fmt.Print("Input destinated number: ")
 	fmt.Scanln(&input)
-
-	for i := 1; i <= input; i++ {
-		fmt.Printf("%d ", i)
-	}
+	printFor(&input)
 }
 func init() {
 	fmt.Println("Hi, chapter6")
