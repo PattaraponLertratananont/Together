19ff1f50a6de459f456bf5c2d99c4fb9d4a94a54
tammarut <tammarut>
2018-12-19 14:17:36 +0700

    [production code]: declare sum  average variable, add sum+=input,calculate average and print both of them.
     Your branch is ahead of 'origin/master' by 3 commits.

--- Learning/Pleum/Chapter7/main.go
+++ Learning/Pleum/Chapter7/main.go
@@ -4,12 +4,18 @@ package main
 import "fmt"
 
 func main() {
-	var input float32
+	var input int
+	sum := 0
+	average := 0
 
 	for i := 1; i <= 10; i++ {
 		fmt.Printf("Input number %d: ", i)
 		fmt.Scanln(&input)
+		sum += input
 	}
+	average = sum / 10
+	fmt.Println("Sum: ", sum)
+	fmt.Println("Average: ", average)
 }
 func init() {
 	fmt.Println("Hi, chapter7")
