fa71debb44773b82e60176a61d9bbd67a460139c
tammarut <tammarut>
2018-12-19 15:30:50 +0700

    [rpoduction code]: declare sumeven,sumodd=0, sumeven+input,sumodd+input and  print sum both of them.

--- Learning/Pleum/Chapter8/main.go
+++ Learning/Pleum/Chapter8/main.go
@@ -6,15 +6,20 @@ import "fmt"
 
 func main() {
 	var input, even, odd int
+	sumeven, sumodd := 0, 0
 	for {
 		fmt.Print("Input number: ")
 		fmt.Scanln(&input)
 		if input%2 == 0 {
+			sumeven += input
 			even++
 		} else {
+			sumodd += input
 			odd++
 		}
 		if input == 0 {
+			fmt.Printf("Sum %d even numbers: %d\n", even-1, sumeven)
+			fmt.Printf("Sum %d odd numbers: %d\n", odd, sumodd)
 			break
 		}
 	}
