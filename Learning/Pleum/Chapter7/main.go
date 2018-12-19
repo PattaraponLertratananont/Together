cff169834a439258e5ac3ae8acf1e833d4570f2e
tammarut <tammarut>
2018-12-19 14:40:01 +0700

    [refactor]: delete average variable and write sum/10 insteaded.

--- Learning/Pleum/Chapter7/main.go
+++ Learning/Pleum/Chapter7/main.go
@@ -4,16 +4,15 @@ package main
 import "fmt"
 
 func main() {
-	var input, sum, average float64
+	var input, sum float64
 
 	for i := 1; i <= 10; i++ {
 		fmt.Printf("Input number %d: ", i)
 		fmt.Scanln(&input)
 		sum += input
 	}
-	average = sum / 10
 	fmt.Println("Sum: ", sum)
-	fmt.Println("Average: ", average)
+	fmt.Println("Average: ", sum/10)
 }
 func init() {
 	fmt.Println("Hi, chapter7")
