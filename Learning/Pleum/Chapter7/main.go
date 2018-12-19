a41b8f3693e11b7eb2d85751532ccd4c679f10c7
tammarut <tammarut>
2018-12-19 14:04:42 +0700

    chapter7 [production code]: input from keyboard done.

---
+++ Learning/Pleum/Chapter7/main.go
@@ -0,0 +1,15 @@
+package main
+
+//เขียนโปรแกรมinput 10ตัว แล้วแล้วแสดงผมรวมทั้งหมดและแสดงค่าเฉลี่ย
+import "fmt"
+
+func main() {
+	var input float32
+
+	fmt.Print("Input number 1: ")
+	fmt.Scanln(&input)
+}
+func init() {
+	fmt.Println("Hi, chapter7")
+	fmt.Println("--------------------")
+}
