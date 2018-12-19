96a677bd1836ea98db6cd68f4cd5352f505376a5
tammarut <tammarut>
2018-12-19 16:38:21 +0700

    [code]: create loop and add if sum=100 stop.

--- Learning/Pleum/Chapter9/main.go
+++ Learning/Pleum/Chapter9/main.go
@@ -5,10 +5,15 @@ import "fmt"
 
 func main() {
 	var input, sum int
-	fmt.Print("Input number (until sum=100): ")
-	fmt.Scanln(&input)
-	sum = sum + input
-	fmt.Println("Sum: ", sum)
+	for {
+		fmt.Print("Input number (until sum=100): ")
+		fmt.Scanln(&input)
+		sum = sum + input
+		fmt.Println("Sum: ", sum)
+		if sum == 100 {
+			break
+		}
+	}
 
 }
 func init() {
