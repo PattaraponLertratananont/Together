7da2fbed199402bfee65deeeaa8317c6096523cd
tammarut <tammarut>
2018-12-20 16:44:25 +0700

    [code]: create if check middle of even number and take it to the forloop.

--- Learning/Pleum/Chapter12/main.go
+++ Learning/Pleum/Chapter12/main.go
@@ -10,9 +10,15 @@ func main() {
 		fmt.Printf("Input number %d: ", i+1)
 		fmt.Scanln(&box[i])
 	}
+	for i := 1; i < len(box)-1; i++ {
+		if box[i-1]%2 == 0 && box[i+1]%2 == 0 {
+			fmt.Print(" ", box[i])
+		}
+	}
 
 }
 func init() {
 	fmt.Println("Hi, chapter 12")
+
 	fmt.Println("-------------------------")
 }
