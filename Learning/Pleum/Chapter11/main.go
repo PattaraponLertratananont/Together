4970b170cdda3e8fa852a9dba23ac8a74ab86d58
tammarut <tammarut>
2018-12-20 12:41:44 +0700

    [code]: create 1 if/else ->peatagolus.

--- Learning/Pleum/Chapter11/main.go
+++ Learning/Pleum/Chapter11/main.go
@@ -1,10 +1,13 @@
 package main
 
 //เขียนโปรแกรมinputเลข 3ตัว แสดงผลว่าเป็นสามเหลี่ยมมุมฉากหรือไม่?
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	var x, y, z int
+	var x, y, z float64
 
 	fmt.Print("X: ")
 	fmt.Scanln(&x)
@@ -12,6 +15,12 @@ func main() {
 	fmt.Scanln(&y)
 	fmt.Print("Z: ")
 	fmt.Scanln(&z)
+
+	if math.Pow(x, 2)+math.Pow(y, 2) == math.Pow(z, 2) {
+		fmt.Println("X, Y, Z are side of right triangle.")
+	} else {
+		fmt.Println("No, it's not side of right triangle. ")
+	}
 }
 func init() {
 	fmt.Println("Hi, chapter11")
