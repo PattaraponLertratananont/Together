d537ef40df41adeb2392ed03509251ab434891cb
tammarut <tammarut>
2018-12-21 11:03:59 +0700

    [code]: declare p variable, create whileloop2 condtion and if check even/odd number (input%p), then print p as a factor, input divided by p and set p=2 again.

--- Learning/Pleum/Chapter14/main.go
+++ Learning/Pleum/Chapter14/main.go
@@ -4,9 +4,22 @@ package main
 import "fmt"
 
 func main() {
-	input := 0
+	input, p := 0, 2
 	fmt.Print("Input number: ")
 	fmt.Scanln(&input)
+	fmt.Printf("factor %d= ", input)
+
+	for input > 1 && p < input {
+		if input%p == 0 {
+			fmt.Printf("%d x ", p)
+			input = input / p
+			p = 2
+		} else {
+			p++
+		}
+	}
+	fmt.Printf("%d \n", p)
+
 }
 func init() {
 	fmt.Println("Hi, chapter14")
