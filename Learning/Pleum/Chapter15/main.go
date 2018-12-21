4112ef111f45846647486e9852d12756ab9e2043
tammarut <tammarut>
2018-12-21 11:52:06 +0700

    [code]: we want the same หรม.(first,second) so then find หรม.second by make additional condition at for and if.

--- Learning/Pleum/Chapter15/main.go
+++ Learning/Pleum/Chapter15/main.go
@@ -4,24 +4,25 @@ package main
 import "fmt"
 
 func main() {
-	var first int
+	var first, second int
 	p := 2
 	sum := 1
 	fmt.Print("Input first number: ")
 	fmt.Scanln(&first)
-	// fmt.Print("Input second number: ")
-	// fmt.Scanln(&second)
+	fmt.Print("Input second number: ")
+	fmt.Scanln(&second)
 
-	for first > 1 && p < first {
-		if first%p == 0 {
+	for first > 1 && p < first && second > 1 && p < second {
+		if first%p == 0 && second%p == 0 {
 			sum = sum * p
 			first = first / p
+			second = second / p
 			p = 2
 		} else {
 			p++
 		}
 	}
-	fmt.Println(sum)
+	fmt.Println("Greatest common divisor: ", sum)
 }
 func init() {
 	fmt.Println("Hi, chapter15")
