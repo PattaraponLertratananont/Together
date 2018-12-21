9cba9936e3a6c327c9d59747a7a9dc491757fbeb
tammarut <tammarut>
2018-12-21 10:25:07 +0700

    chapter14[code]: input 1 number from keyboard.

---
+++ Learning/Pleum/Chapter14/main.go
@@ -0,0 +1,14 @@
+package main
+
+//เขียนโปรแกรมรับinputเลข 1ตัวแ้วแสดงผลการแยกตัวประกอบ
+import "fmt"
+
+func main() {
+	input := 0
+	fmt.Print("Input number: ")
+	fmt.Scanln(&input)
+}
+func init() {
+	fmt.Println("Hi, chapter14")
+	fmt.Println("--------------------------------")
+}
