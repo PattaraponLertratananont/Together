ea748a93ef2a8406025070a87582e449d81e19cb
tammarut <tammarut>
2018-12-24 22:40:44 +0700

    chapter16[code]: create if/elseif/else to saperate prime number by mod 2-3-5.

--- Learning/Pleum/chapter16/main.go
+++ Learning/Pleum/chapter16/main.go
@@ -8,7 +8,15 @@ func main() {
 	fmt.Print("Input number: ")
 	fmt.Scanln(&input)
 
+	if input == 2 || input == 3 || input == 5 {
+		fmt.Println("It's a prime number.")
+	} else if input%2 != 0 && input%3 != 0 && input%5 != 0 {
+		fmt.Println("It's a prime number.")
+	} else {
+		fmt.Println("It is not prime number.")
+	}
 }
+
 func init() {
 	fmt.Println("Hi, chapter16")
 	fmt.Println("--------------------------")
