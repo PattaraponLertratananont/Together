f468f4101078c8ff5ee168f3940829c6c5ddaf77
tammarut <tammarut>
2018-12-24 16:34:55 +0700

    struct[code]: form to Json by change field to uppercase, jason.Marshal and convert to string before print.

--- Learning/Pleum/exercise/struct/student.go
+++ Learning/Pleum/exercise/struct/student.go
@@ -1,45 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 type student struct {
-	id        string
-	firstname string
-	lastname  string
-	no        int
-	phone     string
+	Id        string
+	Firstname string
+	Lastname  string
+	No        int
+	Phone     string
 }
 
 func main() {
 	studentLists := []student{}
 
 	student1 := student{
-		id:        "605233000",
-		firstname: "Tammarut",
-		lastname:  "Nualmeechue",
-		no:        5,
-		phone:     "087-1542740",
+		Id:        "605233000",
+		Firstname: "Tammarut",
+		Lastname:  "Nualmeechue",
+		No:        5,
+		Phone:     "087-1542740",
 	}
 	student2 := student{
-		id:        "5099992322",
-		firstname: "Bruce",
-		lastname:  "Lee",
-		no:        6,
-		phone:     "094-554618",
+		Id:        "5099992322",
+		Firstname: "Bruce",
+		Lastname:  "Lee",
+		No:        6,
+		Phone:     "094-554618",
 	}
 	student3 := student{
-		id:        "1011101010",
-		firstname: "The",
-		lastname:  "Flash",
-		no:        99,
-		phone:     "011-1111111",
+		Id:        "1011101010",
+		Firstname: "The",
+		Lastname:  "Flash",
+		No:        99,
+		Phone:     "011-1111111",
 	}
-	fmt.Println("Before: ", studentLists)
 	studentLists = append(studentLists, student1)
 	studentLists = append(studentLists, student2)
 	studentLists = append(studentLists, student3)
 
-	fmt.Println("After: ", studentLists)
+	studentListsJson, err := json.Marshal(studentLists)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(studentListsJson))
 }
 func init() {
 	fmt.Println("Hi, student")
