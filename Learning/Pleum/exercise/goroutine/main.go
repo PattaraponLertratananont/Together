80f47e1f1037136bd5aa73d7c9b62e34b0781963
tammarut <tammarut>
2018-12-24 17:15:27 +0700

    goroutine[code]: create 4 function and every function delay 1 second.

--- Learning/Pleum/exercise/goroutine/main.go
+++ Learning/Pleum/exercise/goroutine/main.go
@@ -2,7 +2,24 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
+
+func buyGkassesMinimart() {
+	time.Sleep(1 * time.Second)
+	fmt.Println("ซื้อแว่นตา ที่มินิมาท")
+}
+func buyWatchesCentral() {
+	time.Sleep(1 * time.Second)
+	fmt.Println("ซื้อนาฬิกา ที่เซนทรัล")
+}
+func buyFruitsParagon() {
+	time.Sleep(1 * time.Second)
+}
+func buyCarHonda() {
+	time.Sleep(1 * time.Second)
+	fmt.Println("ซื้อรถ ที่ฮอนด้า")
+}
 
 func main() {
 	buyGkassesMinimart()
