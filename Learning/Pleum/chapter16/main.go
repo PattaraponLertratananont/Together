9fa523a5b56dd8e04607cbd19f9bcb0871ce57b3
tammarut <tammarut>
2018-12-24 22:26:59 +0700

    chapter16[code]: input 1 number from keyboard.

---
+++ Learning/Pleum/chapter16/main.go
@@ -0,0 +1,15 @@
+package main
+
+//เขียนโปรแกรมรับinputเลข 1ตัว แสดงผลว่าเป็นจำนวนเฉพาะ(Prime number)หรือไม่
+import "fmt"
+
+func main() {
+	var input int
+	fmt.Print("Input number: ")
+	fmt.Scanln(&input)
+
+}
+func init() {
+	fmt.Println("Hi, chapter16")
+	fmt.Println("--------------------------")
+}
