138ded3f75baac853b3e6282ba666e5eeb92fa58
tammarut <tammarut>
2018-12-24 22:46:18 +0700

    chapter16[refactor]:gather if onlyone condition.

--- Learning/Pleum/chapter16/main.go
+++ Learning/Pleum/chapter16/main.go
@@ -8,9 +8,7 @@ func main() {
 	fmt.Print("Input number: ")
 	fmt.Scanln(&input)
 
-	if input == 2 || input == 3 || input == 5 {
-		fmt.Println("It's a prime number.")
-	} else if input%2 != 0 && input%3 != 0 && input%5 != 0 {
+	if input%2 != 0 && input%3 != 0 && input%5 != 0 || input == 2 || input == 3 || input == 5 {
 		fmt.Println("It's a prime number.")
 	} else {
 		fmt.Println("It is not prime number.")
