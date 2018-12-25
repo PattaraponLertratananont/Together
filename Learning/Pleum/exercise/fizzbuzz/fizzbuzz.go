7f467390fd1b168c01c889ca2853fd341e7ccedd
tammarut <tammarut>
2018-12-25 12:29:50 +0700

    fizzbuzz[refactor]: remain only 3 ifconditionand the others case convert to string then return itself.

--- Learning/Pleum/exercise/fizzbuzz/fizzbuzz.go
+++ Learning/Pleum/exercise/fizzbuzz/fizzbuzz.go
@@ -1,16 +1,14 @@
 package fizzbuzz
 
+import "strconv"
+
 func Fizzbuzz(number int) string {
-	if number == 2 {
-		return "2"
-	} else if number == 3 {
+	if number == 3 {
 		return "Fizz"
-	} else if number == 4 {
-		return "4"
 	} else if number == 5 {
 		return "Buzz"
 	} else if number == 15 {
 		return "FizzBuzz"
 	}
-	return "1"
+	return strconv.Itoa(number)
 }
