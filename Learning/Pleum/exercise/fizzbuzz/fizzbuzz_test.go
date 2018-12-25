6dd3046d8507f119fd142fad188bf655d8c57da0
tammarut <tammarut>
2018-12-25 11:59:21 +0700

    fizzbuzz[code]: add 1 testcase and create if number=2 to return 2.

--- Learning/Pleum/exercise/fizzbuzz/fizzbuzz_test.go
+++ Learning/Pleum/exercise/fizzbuzz/fizzbuzz_test.go
@@ -9,3 +9,10 @@ func TestFizzBuzzOne(t *testing.T) {
 		t.Errorf("Expect 1 but it gets: %q", result)
 	}
 }
+func TestFizzBuzzTwo(t *testing.T) {
+	result := Fizzbuzz(2)
+
+	if result != "2" {
+		t.Errorf("Expect 2 but it gets: %q", result)
+	}
+}
