0d2802dbc2303fe4372af2d3ff1ee5d72d82ac70
tammarut <tammarut>
2018-12-26 11:54:48 +0700

    chapter19[code]: input n from keyboard.

---
+++ Learning/Pleum/chapter19/main.go
@@ -0,0 +1,15 @@
+package main
+
+//เขียนโปรแกรมinputเลข 1ตัว แล้วแสดงดอกจัน(*)เป็นสี่เหลี่ยม nxn
+import "fmt"
+
+func main() {
+	var input int
+	fmt.Print("Input n: ")
+	fmt.Scanln(&input)
+}
+func init() {
+	fmt.Println("Hi, chapter19")
+	fmt.Println("——————————————————————")
+
+}
