993b1162c22d83ebc2e00826aa3c1b9fff8c38fb
tammarut <tammarut>
2018-12-26 10:54:09 +0700

    chapter17[code]: print answer as after square root it.

--- Learning/Pleum/chapter17/main.go
+++ Learning/Pleum/chapter17/main.go
@@ -1,12 +1,17 @@
 package main
 
 //เขียนโปรแกรมรับเลขบวกinput 1ตัว แล้วแสดงsquare root หากใส่ติดลบให้ป้อนใหม่จนกว่าจะใส่บวก
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	var input int
+	var input float64
 	fmt.Print("Input 1 number: ")
 	fmt.Scanln(&input)
+
+	fmt.Printf("Square root %d = %.2f \n", int32(input), math.Sqrt(input))
 }
 func init() {
 	fmt.Println("Hi, chapter 17 ")
