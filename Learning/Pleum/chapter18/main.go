89057d976236714781a3f040c6db3fa4ccdd0271
tammarut <tammarut>
2018-12-26 11:30:58 +0700

    chapter18[code]: input amount stars.

---
+++ Learning/Pleum/chapter18/main.go
@@ -0,0 +1,15 @@
+package main
+
+//เขียนโปรแกรมรับเลขinput 1ตัวแล้วแสดงดอกจัน(*)ตามจำนวนinputที่ป้อน
+import "fmt"
+
+func main() {
+	var input int
+	fmt.Print("Input amount of stars: ")
+	fmt.Scanln(&input)
+}
+func init() {
+	fmt.Println("Hi, chapter18")
+	fmt.Println("——————————————————————")
+
+}
