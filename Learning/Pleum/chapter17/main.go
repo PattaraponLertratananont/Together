e513214c4c7c9bb525651cd3484402f10b441c60
tammarut <tammarut>
2018-12-26 10:48:39 +0700

    chapter17[code]: input 1 number from keyboard.

---
+++ Learning/Pleum/chapter17/main.go
@@ -0,0 +1,14 @@
+package main
+
+//เขียนโปรแกรมรับเลขบวกinput 1ตัว แล้วแสดงsquare root หากใส่ติดลบให้ป้อนใหม่จนกว่าจะใส่บวก
+import "fmt"
+
+func main() {
+	var input int
+	fmt.Print("Input 1 number: ")
+	fmt.Scanln(&input)
+}
+func init() {
+	fmt.Println("Hi, chapter 17 ")
+	fmt.Println("------------------------")
+}
