4c5ff23c12643dd71ba6924e377f5a52ff9a3b1b
tammarut <tammarut>
2019-01-07 00:15:49 +0700

    chapter20[code]: input from keyboard .

---
+++ Learning/Pleum/chapter20/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+//เขียนโปรแกรมinputเลข 1ตัว แล้วแสดงดอกจัน(*)เป็นสี่เหลี่ยม nxn แบบตารางหมากรุก
+func main() {
+	var keyboard int
+	fmt.Print("Input number: ")
+	fmt.Scanln(&keyboard)
+}
