24901977fdb2f7488c9af3dcc8245c3a2d779809
tammarut <tammarut>
2019-01-08 12:10:48 +0700

    handon#3: use Mutex to solve race_conditio, there're only 3 steps.

--- Learning/Pleum/ninja_lv9/handon#3/main.go
+++ Learning/Pleum/ninja_lv9/handon#3/main.go
@@ -11,17 +11,20 @@ func main() {
 	fmt.Println("Start goroutine:", runtime.NumGoroutine())
 
 	var wg sync.WaitGroup
+	var mu sync.Mutex
 	incrementer := 0
 	const gs = 100
 
 	wg.Add(gs)
 	for i := 1; i <= gs; i++ {
 		go func() {
+			mu.Lock()
 			v := incrementer
 			runtime.Gosched()
 			v++
 			incrementer = v
 			fmt.Println(incrementer)
+			mu.Unlock()
 			wg.Done()
 		}()
 		fmt.Println("Middle goroutine:", runtime.NumGoroutine())
